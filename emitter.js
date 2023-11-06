274e0edb3dd8c0b3ddfd9268bfb0d5d4fec80f79
Vladyslav Diachenko <basher207vd@gmail.com>
2023-11-07 00:58:05 +0200

    1 test failed. Test: CatchErrorSpy

--- emitter.js
+++ emitter.js
@@ -1,1 +1,15 @@
-module.exports = // Your code here
+const { interval, map, take, merge, first} = require("rxjs");
+
+const customErrorObservable = interval(5500).pipe(
+    first(),
+    map(x => {throw new Error("Explosion!")})
+);
+
+const everySecondObservable = interval(1000).pipe(
+    take(7),
+    map(x => 'tick')
+);
+
+const result = merge(everySecondObservable, customErrorObservable);
+
+module.exports = result;
--- main.js
+++ main.js
@@ -1,6 +1,14 @@
+const { catchError, map, of} = require("rxjs");
 const clock$ = require("./emitter");
 
-const subscription = clock$
-  // Your code here
+const subscription = clock$.pipe(
+  map((x, index) => (x === 'tick' && index % 2 !== 0) ? 'tock' : x),
+  catchError(err => of(err.message))
+)
+.subscribe({
+  next: x => console.log(x),
+  error: (err) => console.log(err),
+  complete: () => console.log('Observer got a complete notification')
+});
 
 module.exports = subscription;
